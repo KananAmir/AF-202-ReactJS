bf56d1a81d75cf3ee033f9819a8020907e871fda
Kanan Amirov <amirovknn@gmail.com>
2022-12-21 20:38:38 +0400

    fix

--- 06.styling-in-react/src/App.jsx
+++ 06.styling-in-react/src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Card from "./components/card";
-import Mzah from "./assets/izah";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Header />
       <hr />
       <Card />
-      <Mzah />
       <hr />
       {/* <Footer /> */}
     </div>
