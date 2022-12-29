668277cbbf394f73da6654444da4f2bd705713f0
Kanan Amirov <amirovknn@gmail.com>
2022-12-29 17:38:57 +0400

    sorting by string and number values

--- 11.sort.filter/src/App.js
+++ 11.sort.filter/src/App.js
@@ -1,6 +1,7 @@
 import "./App.scss";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Button } from "antd";
 
 function App() {
   const [orders, setOrders] = useState([]);
@@ -19,21 +20,59 @@ function App() {
       setOrders(newOrders);
     });
   };
+
+  const handleSortByShipnameAscending = () => {
+    // setOrders(
+    //   orders.sort((a, b) => (a.shipName > b.shipName ? 1 : -1)).slice()
+    // );
+    // setOrders([...orders.sort((a, b) => (a.shipName > b.shipName ? 1 : -1))]);
+
+    setOrders([...orders.sort((a, b) => a.shipName.localeCompare(b.shipName))]);
+  };
+  const handleSortByShipnameDescending = () => {
+    // setOrders([...orders.sort((a, b) => (b.shipName > a.shipName ? 1 : -1))]);
+    setOrders([...orders.sort((a, b) => b.shipName.localeCompare(a.shipName))]);
+
+  };
+
+  const handleSortByFreightAscending = () => {
+    setOrders([...orders.sort((a, b) => a.freight - b.freight)]);
+  };
+
+  const handleSortByFreightDescending = () => {
+    setOrders([...orders.sort((a, b) => b.freight - a.freight)]);
+  };
+
   return (
     <div className="App">
       <input type="text" onChange={(e) => handleChange(e)} />
 
-      {console.log(orders)}
+      <br />
+      <br />
+      <Button onClick={handleSortByShipnameAscending}>
+        Sort by ShipName - asc
+      </Button>
+      <Button onClick={handleSortByShipnameDescending}>
+        Sort by ShipName - des
+      </Button>
+      <Button onClick={handleSortByFreightAscending}>
+        Sort by Freight - asc
+      </Button>
+      <Button onClick={handleSortByFreightDescending}>
+        Sort by Freight - des
+      </Button>
+      <br />
+      <br />
       <table>
         <thead>
           <th>id</th>
-          <th>shipName</th>
-          <th>freight</th>
+          <th>Ship Name</th>
+          <th>Freight</th>
         </thead>
         <tbody>
           {orders.map((q) => {
             return (
-              <tr>
+              <tr key={q.id}>
                 <td>{q.id}</td>
                 <td>{q.shipName}</td>
                 <td>{q.freight}</td>
