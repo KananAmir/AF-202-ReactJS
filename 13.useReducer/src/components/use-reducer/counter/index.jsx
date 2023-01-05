13ce86780e9cec641f0bfcc957fe4dba0fbe08cb
Kanan Amirov <amirovknn@gmail.com>
2023-01-05 14:43:19 +0400

    useReducer recap

--- 13.useReducer/src/components/use-reducer/counter/index.jsx
+++ 13.useReducer/src/components/use-reducer/counter/index.jsx
@@ -6,6 +6,9 @@ import React, { useReducer } from "react";
 const actions = {
   INCREMENT: "INCREMENT",
   DECREMENT: "DECREMENT",
+  INCREMENT_BY_3: "INCREMENT_BY_3",
+  SET_INPUT_VALUE: "SET_INPUT_VALUE",
+  INCREMENT_BY_INPUT: "INCREMENT_BY_INPUT",
 };
 
 const reducer = (state, action) => {
@@ -14,6 +17,12 @@ const reducer = (state, action) => {
       return { ...state, count: state.count + 1 };
     case actions.DECREMENT:
       return { count: state.count - 1 };
+    case actions.INCREMENT_BY_3:
+      return { count: state.count + action.payload };
+    case actions.SET_INPUT_VALUE:
+      return { ...state, inputValue: action.payload };
+    case actions.INCREMENT_BY_INPUT:
+      return { ...state, count: state.count + +action.payload };
     default:
       return state;
   }
@@ -26,12 +35,27 @@ const CounterWithUseReducer = () => {
   const handleDecrement = () => {
     dispatch({ type: "DECREMENT" });
   };
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const handleIncrementBy3 = () => {
+    dispatch({ type: "INCREMENT_BY_3", payload: 3 });
+  };
+  const handleByInputValue = () => {
+    dispatch({ type: "INCREMENT_BY_INPUT", payload: state.inputValue });
+  };
+
+  const [state, dispatch] = useReducer(reducer, { count: 0, inputValue: "" });
   return (
     <div>
       <button onClick={handleIncrement}>Increment</button>
       {state.count}
       <button onClick={handleDecrement}>Decrement</button>
+      <input
+        onChange={(e) =>
+          dispatch({ type: "SET_INPUT_VALUE", payload: e.target.value })
+        }
+        type="text"
+      />
+      <button onClick={handleByInputValue}>Increment By Input Value</button>
+      <button onClick={handleIncrementBy3}>Increment by 3</button>
     </div>
   );
 };
